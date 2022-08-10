29091f9c206d937242c0189a118125ec770551e5
PleaseWorkMod <pleaseworkmod@outlook.com>
2022-08-10 21:16:58 +0200

    Add animation set generation

--- src/main/Generator.java
+++ src/main/Generator.java
@@ -20,7 +20,8 @@ import java.util.stream.Stream;
 
 public class Generator implements Runnable {
     private static final String BASE_DIRECTORY = "Meshes\\Actors\\Character\\Animations\\DynamicAnimationReplacer\\_CustomConditions";
-    private static final int BASE_INDEX = 4096;
+    private static final int BASE_IDLE_INDEX = 4096;
+    private static final int BASE_MOVEMENT_INDEX = 256;
     private static final String FEMALE_FOLDER_NAME = "female";
     private static final String CONDITIONS_FILE_NAME = "_conditions.txt";
     private static final String CONDITIONS_PC =
@@ -40,8 +41,9 @@ public class Generator implements Runnable {
             "IsFemale() AND\n" +
                     "NOT IsChild() AND\n" +
                     "Random(%s)";
-    private static final String ANIMATION_FILE_NAME = "mt_idle.hkx";
-    private static final String ANIMATION_FILES_DIRECTORY = "."; //replace with folder that contains .hkx files, takes animations in same folder as executable by default
+    private static final String IDLE_FILE_NAME = "mt_idle.hkx";
+    private static final String DYNAMIC_IDLES_DIRECTORY = "dynamic_idles"; //replace with folder that contains .hkx files, takes animations in same folder as executable by default
+    private static final String DYNAMIC_SETS_DIRECTORY = "dynamic_sets"; //replace with folder that contains .hkx files, takes animations in same folder as executable by default
 
     private static final Logger logger = Main.logger;
 
@@ -69,7 +71,7 @@ public class Generator implements Runnable {
     @Override
     public void run() {
         createBaseDirectory();
-        createAnimationFolders();
+        createFiles();
     }
 
     private void createBaseDirectory() {
@@ -85,48 +87,107 @@ public class Generator implements Runnable {
         }
     }
 
-    private void createAnimationFolders() {
-        File folder = new File(ANIMATION_FILES_DIRECTORY);
+    private void createFiles() {
+        if(Files.exists(Paths.get(DYNAMIC_SETS_DIRECTORY))) {
+            createSetConditions();
+        } else {
+            logger.info("Movement folder " + DYNAMIC_SETS_DIRECTORY + " not found.");
+        }
+
+        if(Files.exists(Paths.get(DYNAMIC_IDLES_DIRECTORY))) {
+            createIdleConditions();
+        } else {
+            logger.info("Idles folder " + DYNAMIC_SETS_DIRECTORY + " not found.");
+        }
+    }
+
+    private void createIdleConditions() {
+        File folder = new File(DYNAMIC_IDLES_DIRECTORY);
         File[] listOfFiles = folder.listFiles();
 
         if(listOfFiles == null) {
-            throw new CustomRuntimeException("Could not find any animation files");
+            throw new CustomRuntimeException("Could not get list of idle animation files");
         }
 
-        int hkxCount = 0;
-        for (File listOfFile : listOfFiles) {
-            if (listOfFile.isFile() && isHkxFile(listOfFile)) {
-                hkxCount++;
-                logger.info("Found hkx file: " + listOfFile.getName());
-                createAnimationFolder(String.valueOf(BASE_INDEX + hkxCount - 1), listOfFile, 1.0 / hkxCount);
+        int animationCount = 0;
+        for (File animationFile : listOfFiles) {
+            if (animationFile.isFile() && isHkxFile(animationFile)) {
+                logger.info("Found idle file: " + animationFile.getName());
+                String conditionFolder = String.valueOf(BASE_IDLE_INDEX + animationCount);
+                animationCount++;
+                double chance = 1.0 / animationCount;
+                createConditionFolder(conditionFolder);
+                createConditionFile(conditionFolder, chance);
+                copyToConditionFolder(animationFile, conditionFolder, IDLE_FILE_NAME);
             }
         }
     }
 
-    private void createAnimationFolder(String folderName, File animationFile, double chance) {
-        deleteFolderIfExists(Paths.get(BASE_DIRECTORY + "\\" + folderName));
-        Path path = Paths.get(BASE_DIRECTORY + "\\" + folderName + "\\" + FEMALE_FOLDER_NAME);
-        Path targetFilePath = Paths.get(BASE_DIRECTORY + "\\" + folderName + "\\" + FEMALE_FOLDER_NAME + "\\" + ANIMATION_FILE_NAME);
-        try {
-            logger.info("Creating folder " + path.toAbsolutePath());
-            Files.createDirectories(path);
-            logger.info("Copying animation file " + animationFile.toPath().toAbsolutePath() + " to " + targetFilePath.toAbsolutePath());
-            Files.copy(animationFile.toPath(), targetFilePath);
-            createConditionsFile(BASE_DIRECTORY + "\\" + folderName, chance);
-        } catch (IOException e) {
-            throw new CustomRuntimeException("Could not create Animation Folder " + path.toAbsolutePath(), e);
+    private void createSetConditions() {
+        File folder = new File(DYNAMIC_SETS_DIRECTORY);
+        File[] listOfFolders = folder.listFiles();
+
+        if(listOfFolders == null) {
+            throw new CustomRuntimeException("Could not get list of set animation folders");
+        }
+
+        int folderCount = 0;
+        for (File movementFolder : listOfFolders) {
+            if(movementFolder.isDirectory()) {
+                logger.info("Found set folder: " + movementFolder.getName());
+
+                String conditionFolder = String.valueOf(BASE_MOVEMENT_INDEX + folderCount);
+                folderCount++;
+                double chance = 1.0 / folderCount;
+                createConditionFolder(conditionFolder);
+                createConditionFile(conditionFolder, chance);
+
+                File[] listOfAnimations = movementFolder.listFiles();
+                if(listOfAnimations == null) {
+                    throw new CustomRuntimeException("Could not get list of set animation files");
+                }
+                for (File animationFile : listOfAnimations) {
+                    if (animationFile.isFile() && isHkxFile(animationFile)) {
+                        logger.info("Found hkx file: " + animationFile.getName());
+                        copyToConditionFolder(animationFile, conditionFolder, animationFile.getName());
+                    }
+                }
+            }
         }
     }
-    private void createConditionsFile(String path, double chance) {
+
+    private void createConditionFolder(String conditionFolder) {
+        deleteFolderIfExists(Paths.get(BASE_DIRECTORY + "\\" + conditionFolder));
+        Path path = Paths.get(BASE_DIRECTORY + "\\" + conditionFolder + "\\" + FEMALE_FOLDER_NAME);
         try {
-            logger.info("Creating conditions file " + path + "\\" + CONDITIONS_FILE_NAME);
-            FileOutputStream file = new FileOutputStream(path + "\\" + CONDITIONS_FILE_NAME);
+            logger.info("Creating directory " + path.toAbsolutePath());
+            Files.createDirectories(path);
+        } catch (IOException e) {
+            throw new CustomRuntimeException("Could not create condition Folder " + path.toAbsolutePath(), e);
+        }
+    }
+
+    private void copyToConditionFolder(File animationFile, String conditionFolder, String targetFileName) {
+        Path targetFilePath = Paths.get(BASE_DIRECTORY + "\\" + conditionFolder + "\\" + FEMALE_FOLDER_NAME + "\\" + targetFileName);
+        try {
+            logger.info("Copying animation file " + animationFile.toPath().toAbsolutePath() + " to " + targetFilePath.toAbsolutePath());
+            Files.copy(animationFile.toPath(), targetFilePath);
+        } catch (IOException e) {
+            throw new CustomRuntimeException("Could not copy animation file to " + targetFilePath.toAbsolutePath(), e);
+        }
+    }
+
+    private void createConditionFile(String conditionFolder, double chance) {
+        Path path = Paths.get(BASE_DIRECTORY, conditionFolder, CONDITIONS_FILE_NAME);
+        try {
+            logger.info("Creating conditions file " + path);
+            FileOutputStream file = new FileOutputStream(path.toString());
             BufferedOutputStream bufferedWriter = new BufferedOutputStream(file);
             logger.info("Using chance " + trimNumber(chance));
             bufferedWriter.write(getConditionsText(chance).getBytes());
             bufferedWriter.close();
         } catch (IOException e) {
-            throw new CustomRuntimeException("Could not create Animation Conditions File " + path + CONDITIONS_FILE_NAME, e);
+            throw new CustomRuntimeException("Could not create Animation Conditions File " + path, e);
         }
     }
 
